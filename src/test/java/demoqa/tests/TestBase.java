dedf123bef78f749b6b4d47660a824e3d1c07aaa
Andrey Kulikov <9230697@gmail.com>
2021-12-02 10:29:09 +0300

    добавил allure и обновил build.gradle

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -1,18 +1,22 @@
 package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import demoqa.pages.RegistrationPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Selenide.open;
 
-public class TestBase extends TestData{
+public class TestBase extends TestData {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
     @BeforeAll
     static void beforeAll () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
     @BeforeEach
